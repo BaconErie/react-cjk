68290340ccde4a024eace2b1e5d28f6a86a705c7
BaconErie <baconerie0001@gmail.com>
2024-02-13 21:22:52 -0500

    Basic character rendering

---
+++ react-cjk.js
@@ -0,0 +1,80 @@
+import { useEffect, useState } from 'react';
+import styles from './CharacterSVG.module.css';
+
+export function CharacterSVG({character, source, strokesDisplayed, onStrokeComplete, ...otherProps}) {
+  const [ strokeData, setStrokeData ] = useState([]);
+  const [ displayedSVG, setDisplayedSVG ] = useState([]);
+
+  async function onCharacterChange() {
+    let urlToUse = '';
+
+    switch (source) {
+      case 'zh-hans':
+        urlToUse = 'https://raw.githubusercontent.com/skishore/makemeahanzi/master/graphics.txt';
+        break;
+      
+      case 'zh-hant':
+        urlToUse = 'https://raw.githubusercontent.com/parsimonhi/animCJK/master/graphicsZhHant.txt';
+        break;
+      
+      case 'jp':
+        urlToUse = 'https://raw.githubusercontent.com/parsimonhi/animCJK/master/graphicsJa.txt';
+        break;
+
+      case 'ko':
+        urlToUse = 'https://raw.githubusercontent.com/parsimonhi/animCJK/master/graphicsKo.txt';
+        break;       
+      
+      default:
+        urlToUse = source;
+        break;
+    }
+
+    const characterData = await (await fetch(urlToUse, {cache: 'force-cache'})).text();
+    const characterDataRows = characterData.split('\n');
+
+    let graphicsJsonArray = [];
+    
+    for (const rowString of characterDataRows) {
+      if (rowString)
+        graphicsJsonArray.push(JSON.parse(rowString))
+    }
+
+    let newStrokeData;
+    for (const json of graphicsJsonArray) {
+      if (json.character == character) {
+        newStrokeData = json.strokes;
+        break;
+      }
+    }
+
+    setStrokeData(newStrokeData);
+
+    await onStrokesDisplayedChange(newStrokeData);
+}
+
+  useEffect(() => {
+    onCharacterChange();
+  }, [character]);
+
+  async function onStrokesDisplayedChange(strokeData) {
+    let newDisplayedSvg = [];
+
+    for (const [key, value] of Object.entries(strokesDisplayed)) {
+      newDisplayedSvg.push(strokeData[key]);
+    }
+    setDisplayedSVG(newDisplayedSvg);
+  }
+
+  useEffect(() => {
+    onStrokesDisplayedChange(strokeData);
+  }, [strokesDisplayed]);
+
+  return (<svg viewBox={'0 0 1024 1024'} {...otherProps}>
+    <g transform={'scale(1, -1) translate(0, -900)'}>
+      {
+        displayedSVG.map((data, index) => <path key={index} d={data} /> )
+      }
+    </g>
+  </svg>)
+}
